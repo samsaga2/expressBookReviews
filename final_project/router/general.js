23e06f1c694db395c49691a0368c46dfa3766cf2
Víctor M. <samsaga2@gmail.com>
2025-01-14 15:07:12 +0100

    Fixed register user bug

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -8,10 +8,11 @@ const public_users = express.Router();
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
+    console.log(req.body);
     // Check if both username and password are provided
     if (username && password) {
         // Check if the user does not already exist
-        if (!doesExist(username)) {
+        if (!isValid(username)) {
             // Add the new user to the users array
             users.push({"username": username, "password": password});
             return res.status(200).json({message: "User successfully registered. Now you can login"});
