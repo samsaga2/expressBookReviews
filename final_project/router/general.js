0af6f477ebb71076b623ac86f3898b195b888bd4
Víctor M. <samsaga2@gmail.com>
2025-01-14 16:57:39 +0100

    Removed test code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -8,7 +8,6 @@ const public_users = express.Router();
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
-    console.log(req.body);
     // Check if both username and password are provided
     if (username && password) {
         // Check if the user does not already exist
