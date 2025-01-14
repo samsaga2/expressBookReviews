c64fef295db8952f85d2954397a6be0aae116fc6
Víctor M. <samsaga2@gmail.com>
2025-01-14 15:11:57 +0100

    Changed task 10 to use async function

--- final_project/test.js
+++ final_project/test.js
@@ -1,40 +1,42 @@
 const axios = require('axios');
 
 // task 10
-axios.get('http://localhost:5000')
-  .then(response => {
-    console.log('Response:', response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error.message);
-  });
+async function fetchData() {
+    try {
+        const response = await axios.get('http://localhost:5000');
+        console.log('Response:', response.data);
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
 
+fetchData();
 
 // task 11
 axios.get('http://localhost:5000/isbn/2')
-  .then(response => {
-    console.log('Response:', response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error.message);
-  });
+    .then(response => {
+        console.log('Response:', response.data);
+    })
+    .catch(error => {
+        console.error('Error:', error.message);
+    });
 
 
 // task 12
 axios.get('http://localhost:5000/author/Unknown')
-  .then(response => {
-    console.log('Response:', response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error.message);
-  });
+    .then(response => {
+        console.log('Response:', response.data);
+    })
+    .catch(error => {
+        console.error('Error:', error.message);
+    });
 
 
 // task 13
 axios.get('http://localhost:5000/title/The Divine Comedy')
-  .then(response => {
-    console.log('Response:', response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error.message);
-  });
+    .then(response => {
+        console.log('Response:', response.data);
+    })
+    .catch(error => {
+        console.error('Error:', error.message);
+    });
